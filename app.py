edb3ee3a4e22978e81c7a1180ab9a25a9d882b03
Nat <70817822+NathalyS19@users.noreply.github.com>
2021-07-15 13:22:27 -0400

    Added aptfile to fix heroku application error

--- app.py
+++ app.py
@@ -50,5 +50,4 @@ def login_validation():
         return render_template('login.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(debug=True)
